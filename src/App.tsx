74e6f6e3d288070f7113cbd46cba63eb19021f93
Antonio Carlos <antoniotx.dev@gmail.com>
2023-02-01 23:23:09 -0300

    style: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import GlobalStyles from './common/styles/global';
+
 function App() {
-  return <div>App works!</div>;
+  return (
+    <>
+      <div>App works!</div>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
